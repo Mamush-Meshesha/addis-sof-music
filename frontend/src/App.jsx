200da0ed773d618d95d07ee56d239d6c1e201db6
Mamush-Meshesha <mamgithub@gmail.com>
2025-07-20 17:59:38 +0300

    initalization and first run

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello adis softwares</h1>
+      <p>This is a song management.</p>
+    </div>
+  );
+}
+
+export default App;
