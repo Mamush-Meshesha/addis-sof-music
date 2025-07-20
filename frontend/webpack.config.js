faba3e9d4170bf6c64cc33ab1e0db36790e31835
Mamush-Meshesha <mamgithub@gmail.com>
2025-07-20 17:19:43 +0300

    webpack configuration added

---
+++ frontend/webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const dotenv = require('dotenv')
+const webpack = require('webpack');
+
+const env = dotenv.config().parsed || {}
+const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`proccess.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+}, {});
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+        publicPath: '/'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    mode: 'development',
+    devServer: {
+        static: './dist',
+        hot: true,
+        open: true,
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude:/node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader','css-loader']
+            },
+            {
+                test: /\.(svg|png|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        }),
+        new webpack.DefinePlugin(envKeys)
+    ]
+}
